b0f7f8fdd8e091c017251131301f0cdea25330f0
Bartosz Ociepka <bociepka@gmail.com>
2019-02-14 00:52:32 +0100

    Funkcjonalny prototyp v. 0.6

--- app/src/main/java/com/example/tramstops/Departure.kt
+++ app/src/main/java/com/example/tramstops/Departure.kt
@@ -5,13 +5,20 @@ class Departure (
     var line_number: String,
     var direction:String,
     var category:String,
-    var actual_time:String
+    var actual_time:String,
+    var delay:String
 ){
 
 
 
     override fun toString(): String {
-        val res:String = "Odjazd: "+display_time+" Linia: "+line_number+"\n "+direction
-        return res
+        var result:String=""
+//        if (delay=="") {
+            result = "Odjazd: " + display_time + " Linia: " + line_number + "\n " + direction +"  "+delay
+//        }
+//        else{
+//            result = "Odjazd: " + display_time + " Linia: " + line_number + "\n " + direction + "  Opóźnienie: "+delay
+//        }
+        return result
     }
 }
